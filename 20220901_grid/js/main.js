6b47bd89a6fbde81b9adbdfef34b0c7bfc9c852e
5h6Sm <5h6sm.github@gmail.com>
2022-09-27 14:04:39 +0900

    start 급식 openAPI

--- 20220901_grid/js/main.js
+++ 20220901_grid/js/main.js
@@ -1,50 +1,37 @@
-// 현재 년, 월, 일 알아내자
-function test(){
-    let today = new Date();   
-
-    let year = today.getFullYear();
-    let month = today.getMonth() + 1;
-    let date = today.getDate();
-    let day = today.getDay();
+function test() {
+    // 현재 년,월,일 알아내자
+    let today = new Date();
+    
+    year = today.getFullYear();
+    month = today.getMonth();   //0~11
+    date = today.getDate();
+    day = today.getDay();   //0: 일, 1: 월, 2: 화, 3: 수, 4: 목, 5: 금, 6: 토
     // 현재 월 마지막날?
-    let lastDate = new Date(year, month, 0).getDate(); //현재 월의 마지막날 : 다음 달의 0일
-    // const currentYear = today.getFullYear();
-    // const currentMonth = today.getMonth();
-    
-    // const lastDate = new Date(currentYear, currentMonth + 1, 0).getDate();
+    let lastDate = new Date(year, month + 1, 0).getDate();  //현재 월의 마지막날: 다음달의 0일
     // 현재 월 1일은 무슨 요일?
-    let firstDate = new Date(year, month, 1).getDay();
-    
-    console.log(`${year}년 ${month}월 ${date}일 ${lastDate}, ${lastDate}`);
+    let firstDay = new Date(year, month, 1).getDay();
+    console.log(`${year}년 ${month + 1}월 ${date}일 ${firstDay}, ${lastDate}`);
 }
 //오늘을 구하자
 let today = new Date();
-//오늘 연 구하기
+//오늘 연 구하자
 let year = today.getFullYear();
-//오늘 월 구하기
-let month = today.getMonth();
+//오늘 월 구하자
+let month = today.getMonth();   //0~11
 month++;
-//오늘 일 구하기
+//오늘 일 구하자
 let date = today.getDate();
-//오늘 요일 구하기
+//오늘 요일 구하자
 let day = today.getDay();
 let days = ['일', '월', '화', '수', '목', '금', '토'];
-
 console.log(`${year}년 ${month}월 ${date}일 ${days[day]}요일`);
 
-//1일 : (오늘 연, 오늘 월, 1) 객체 구하기
-year = 2005;
-month = 5;
-let firstDate = new Date(year, month-1, 1);
-//그 객체의 요일을 구하기
+//1일: (오늘 연, 오늘 월, 1) 객체 구하자
+let firstDate = new Date(year, month - 1, 1);
+//그 객체의 요일 구하자
 let firstDay = firstDate.getDay();
 console.log(days[firstDay]);
-//1일을 html -> js
+//1일을 HTML -> js
 let firstDiv = document.getElementsByClassName("first")[0];
-console.log(firstDiv);
-//grid-column-start : 요일 + 1;
+//grid-column-start: 요일 + 1;
 firstDiv.style.gridColumnStart = firstDay + 1;
-
-
-
-
---
+++ 20220901_grid/js/openapi.js
@@ -0,0 +1,18 @@
+//시도교육청코드 : B10 : 서울특별시교육청
+//표준 학교 코드 : 7010569 : 미림여자정보과학고
+//식사코드 : 2 :중식
+
+//KEY : 
+//ATPT_OFCDC_SC_CODE : 시도교육청코드
+//SD_SCHUL_CODE : 표준학교코드
+//  MMEAL_SC_CODE :식사코드
+//MLSV_YMD : 급식일자
+//MLSV_FROM_YMD : 급십ㄱ시작일자
+//MLSV_TO_YMD : 급식종료일자
+
+//https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010569&MMEAL_SC_CODE=2&MLSV_FROM_YMD=20220927&Type=json
+const KEY = "948a576a81874e00bc91e056962916a8";
+const ATPT_OFCDC_SC_CODE = "B10";
+const SD_SCHUL_CODE = "7010569";
+let MMEAL_SC_CODE = 2; //중식
+let MLSV_FROM_YMD = " 20220927";
